53c6cffd6d8e9cf25a8db21de99d65e05dfed5a1
Heron Slovinski <heronslovinski@hotmail.com>
2022-07-14 17:12:55 -0300

    att

---
+++ Lista03/ex01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Escreva um algoritmo que exiba 20 vezes a mensagem informando qual vez está sendo escrita:
+“Esta é a mensagem 1”, “Esta é a mensagem 2” ... “Esta é a mensagem 20” */
+
+int main()
+{
+
+}
