411f54b40655e320e21d0c42ff31f037c2d529a8
Heron Slovinski <heronslovinski@hotmail.com>
2022-07-15 18:02:09 -0300

    att

--- Lista03/ex09/main.c
+++ Lista03/ex09/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Leia o nome e a idade de 10 pessoas e exiba o nome da pessoa mais nova
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+
+
 }
