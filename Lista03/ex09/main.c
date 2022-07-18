e900262a0a2e6dbe6121aaac12704ff55e92a5a0
Heron Slovinski <heronslovinski@hotmail.com>
2022-07-18 15:25:11 -0300

    att

--- Lista03/ex09/main.c
+++ Lista03/ex09/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Leia o nome e a idade de 10 pessoas e exiba o nome da pessoa mais nova
+/*Escreva um algoritmo que leia 20 números informados pelo usuário e exiba quantos números são
+maiores do que 8*/
 
 int main()
 {
+    float num;
+    int total;
 
+    for ( int i = 0; i < 20; i++ ){
+        printf("Qual e o numero?\n");
+        scanf("%f", &num);
+       if ( num > 8 )
+           total++;
 
+    }
+    printf("%d numeros sao maiores que 8.", total);
 
 }
